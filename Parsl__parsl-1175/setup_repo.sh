e0629934ceee20702971242a89844550e38df1f5
DESKTOP-KI5UI75\Lenovo <18086269699@189.cn>
2025-12-09 11:44:01 +0800

    no message

--- Parsl__parsl-1175/setup_repo.sh
+++ Parsl__parsl-1175/setup_repo.sh
@@ -2,7 +2,7 @@
 # 遇到任何错误则立即退出
 set -e
 
-mkdir -p /testbed
+sudo mkdir -p /testbed
 git clone https://github.com/Parsl/parsl.git /testbed/parsl
 cd /testbed/parsl
 git checkout "fe6d47e0774935e82e14f4cb8123c795f24c627f"
